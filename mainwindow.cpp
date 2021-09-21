527950124ac10a4b0a4d59fdd6289b761dc458c4
KXRXH <74421236+KXRXH@users.noreply.github.com>
2021-09-21 20:25:16 +0700

    some improvements

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,7 +8,6 @@
 #include <iostream>
 #include <fstream>
 
-QStringList FILES_LIST;
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
@@ -35,8 +34,7 @@ MainWindow::MainWindow(QWidget *parent)
     //QTableWidget with files list setup
     FILES_TABLE = new QTableWidget(this);
     FILES_TABLE->move(10, 10);
-    FILES_TABLE->resize(480, 230);
-
+    FILES_TABLE->resize(480, 230);s
     FILES_TABLE->setColumnCount(2);
     FILES_TABLE->horizontalHeader()->hide();
     FILES_TABLE->setEditTriggers(QTableWidget::NoEditTriggers);
@@ -47,9 +45,14 @@ void MainWindow::okBtnEvent()
 {
     QString outFileName = QFileDialog::getSaveFileName(this,
                                             tr("Save file"), "/", tr("Video File (*.mp4)"));
-    std::string cmd = QString("ffmpeg -safe 0 -f concat -i files.txt -c copy %1").arg(outFileName).toStdString();
-    WinExec(cmd.c_str(), SW_HIDE);
-    FILES_TABLE->setRowCount(0);
+    if (outFileName)
+    {    
+        // Creating one big video file from clips (clip's names are insede files.txt)
+        std::string cmd = QString("ffmpeg -safe 0 -f concat -i files.txt -c copy %1").arg(outFileName).toStdString();
+        WinExec(cmd.c_str(), SW_HIDE);
+        // Clearing QTableWidget
+        FILES_TABLE->setRowCount(0);
+    }
 }
 
 
@@ -60,27 +63,28 @@ void MainWindow::browseBtnEvent()
     if (FILES.size())
     {
         QFileInfo fileListInfo("files.txt");
-        bool test = fileListInfo.exists() && fileListInfo.isFile();
         std::ofstream outfile;
-        if (test)
+        //Checking does file exists
+        if (fileListInfo.exists() && fileListInfo.isFile())
         {
-            outfile.open("files.txt", std::ios_base::app);
+            outfile.open("files.txt", std::ios_base::app); // Adding to file
 
         }
         else {
-            outfile.open("files.txt");
+            outfile.open("files.txt"); // Creating new file
         }
         for (auto& path : FILES)
         {
-
-
-            FILES_TABLE->setRowCount(FILES_TABLE->rowCount() + 1);
+            // Filling table
             QFileInfo fi(path);
             QString FileName = fi.fileName();
             QString FileSize = QString("%1 Mb").arg(QString::number(fi.size() / pow(1024, 2)));
+            
+            FILES_TABLE->setRowCount(FILES_TABLE->rowCount() + 1);
             FILES_TABLE->setItem(FILES_TABLE->rowCount() - 1, 0, new QTableWidgetItem(FileName));
             FILES_TABLE->setItem(FILES_TABLE->rowCount() - 1, 1, new QTableWidgetItem(FileSize));
-
+            
+            // Writing paths to file
             outfile << "file '" << path.toStdString() << "'\n";
         }
         outfile.close();
